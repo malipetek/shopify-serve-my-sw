63b80c02dff043843688a92b87eee6f6a3b3986f
muhammet ali petek <malipetek@gmail.com>
2022-12-20 07:58:53 +0300

    add header

--- server/sub-apps/embedded-app/rest-endpoints/proxy.js
+++ server/sub-apps/embedded-app/rest-endpoints/proxy.js
@@ -3,8 +3,9 @@ import { getFile } from '../../../helpers/s3.js';
 export default function (req, res, next) {
   const shop = req.headers['x-shop-domain'];
   const { theme, file } = req.params;
-  const directory = `${shop}/${decodeURIComponent(theme)}`;
-  const filename = `${decodeURIComponent(file)}`;
+  const directory = `${shop}/${theme}`;
+  const filename = `${file}`;
   res.set('content-type', 'text/javascript');
+  res.set('service-worker-allowed', '/');
   res.status(200).send(getFile({ directory, filename }).toString());
 }
